0fae7e834c69105e447beb87045bbb86a00581cb
何冠峰 <hevinci@hotmail.com>
2021-04-02 17:39:01 +0800

    Update demo

--- Project-Patch/Assets/GameScript/Editor/AssetEncrypter.cs
+++ Project-Patch/Assets/GameScript/Editor/AssetEncrypter.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using MotionFramework.Editor;
 
 /// <summary>
 /// 资源打包机密类
 /// </summary>
-public static class AssetEncrypter
+public class AssetEncrypter : IAssetEncrypter
 {
-	public static bool Check(string filePath)
+	bool IAssetEncrypter.Check(string filePath)
 	{
 		// 注意：我们对Entity文件夹内的资源进行了加密
 		return filePath.Contains("/entity/");
 	}
 
-	public static byte[] Encrypt(byte[] fileData)
+	byte[] IAssetEncrypter.Encrypt(byte[] fileData)
 	{
 		int offset = 32;
 		var temper = new byte[fileData.Length + offset];
